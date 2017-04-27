548dbdd8020c6f6e895d914e4d8e255dc2c8c107
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-04-27 18:04:28 +0100

    styling the

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -6,13 +6,13 @@ import ListItem from './ListItem';
 class LibraryList extends Component {
   componentWillMount () {
     const ds = new ListView.DataSource({
-      rowHasChanged: (r1,r2) => r1 !== r2
+      rowHasChanged: (r1, r2) => r1 !== r2
     });
     this.dataSource = ds.cloneWithRows(this.props.libraries);
   }
 
   renderRow(library) {
-      return <ListItem library={library} />
+      return <ListItem library={library}/>
   }
 
 
