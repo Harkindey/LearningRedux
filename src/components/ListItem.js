872931130ab78f61248e80770df8356e2ff9bfbb
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-04-28 23:45:07 +0100

    calling Action creators

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
-    const { titleStyle } = styles
-      return(
+    const { titleStyle } = styles;
+    console.log(this.props); 
+      return (
       <CardSection>
         <Text style={titleStyle}>
           {this.props.library.title}
@@ -20,6 +23,6 @@ const styles = {
     fontSize: 18,
     paddingLeft: 15
   }
-}
+};
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
